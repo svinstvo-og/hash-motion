6640c91b8de2c4a652276cd99dd89992cdcff162
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-05-24 16:38:16 +0200

    First commit

---
+++ src/main/java/motion/hash/HashApplication.java
@@ -0,0 +1,13 @@
+package motion.hash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashApplication.class, args);
+	}
+
+}
